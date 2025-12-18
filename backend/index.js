0bbc8aec2593ff4d6c3104e12307ffe8fc4de84e
Dev Singh <118144997+Devsingh0123@users.noreply.github.com>
2025-12-18 21:19:43 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let port = process.env.PORT || 7000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", // your frontend URL
+app.use(cors({ origin: "https://airbnb-clone-1-nqmg.onrender.com", // your frontend URL
   credentials: true
 }))
 
